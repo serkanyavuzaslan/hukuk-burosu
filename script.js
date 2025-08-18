f89b001dd4716985c5481d792aea0470e402c03a
Serkan <bosyapmaserkan@gmail.com>
2025-08-18 21:54:05 +0300

    İlk commit: proje yükleme

---
+++ script.js
@@ -0,0 +1,18 @@
+function animateCounter(id, end) {
+  let current = 0;
+  const increment = Math.ceil(end / 100);
+  const interval = setInterval(() => {
+    current += increment;
+    if(current >= end) {
+      current = end;
+      clearInterval(interval);
+    }
+    document.getElementById(id).textContent = current;
+  }, 20);
+}
+
+window.addEventListener('load', () => {
+  animateCounter('icra', 7800);
+  animateCounter('vekalet', 874);
+  animateCounter('dava', 4344);
+});
